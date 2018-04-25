cd4da33bd74e624dc02bf1a9346ccd878c8b66ee
Zubin Bhuyan <zoo_bean_13@yahoo.co.in>
2018-04-25 02:06:03 -0400

    Update cgProject2.js

--- finalProject/js/cgProject2.js
+++ finalProject/js/cgProject2.js
@@ -163,8 +163,8 @@ container.appendChild(renderer.domElement);
 var grassTex = THREE.ImageUtils.loadTexture('img/grass.jpg'); 
 grassTex.wrapS = THREE.RepeatWrapping; 
 grassTex.wrapT = THREE.RepeatWrapping; 
-grassTex.repeat.x = 300; 
-grassTex.repeat.y = 225;
+grassTex.repeat.x = 2; 
+grassTex.repeat.y = 3;
 
 var planeGeometry = new THREE.PlaneBufferGeometry( 20, 20, 32, 32 );
 //var planeMaterial = new THREE.MeshLambertMaterial( { color: 0x009090, side: THREE.DoubleSide } )
@@ -178,11 +178,19 @@ plane.position.y = -1.25;
 
 scene.add( plane );
 
-
+//ROOF
+var geometry = new THREE.CylinderGeometry( 0, 1.75, 1, 4 );
+var material = new THREE.MeshPhongMaterial( {color: 0xffff00 , wireframe:false, side: THREE.DoubleSide} );
+var cylinder = new THREE.Mesh( geometry, material );
+cylinder.rotation.y = 0.25 * Math.PI;
+cylinder.position.y = 1.75;
+cylindercastShadow = true;
+//scene.add( cylinder );
+//end-ROOF
 
 // cube setup
 var geometry = new THREE.BoxGeometry( 2, 2.5, 2 );
-
+/*
 var cubeMaterials = [
   new THREE.MeshPhongMaterial({map: new THREE.TextureLoader().load('img/3.jpg'), side: THREE.DoubleSide}), //Right
   new THREE.MeshPhongMaterial({map: new THREE.TextureLoader().load('img/2.jpg'), side: THREE.DoubleSide}), //Left
@@ -191,24 +199,56 @@ var cubeMaterials = [
   new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load('img/1.jpg'), side: THREE.DoubleSide}), //Front
   new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load('img/3.jpg'), side: THREE.DoubleSide}) //Back
 ];
+*/
+var brickTex = THREE.ImageUtils.loadTexture('img/brick.jpg'); 
+brickTex.wrapS = THREE.RepeatWrapping; 
+brickTex.wrapT = THREE.RepeatWrapping; 
+brickTex.repeat.x = 3; 
+brickTex.repeat.y = 3;
 
+var cubeMaterials = [
+  new THREE.MeshPhongMaterial({map: new THREE.TextureLoader().load('img/3.jpg'), side: THREE.DoubleSide}), //Right
+  new THREE.MeshPhongMaterial({map: brickTex, side: THREE.DoubleSide}), //Left
+  new THREE.MeshBasicMaterial({color: 0xF0aa0a, side: THREE.DoubleSide}), //Top
+  new THREE.MeshPhongMaterial({color:0xFFFFFF, side: THREE.DoubleSide}), //Bottom
+  new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load('img/2.jpg'), side: THREE.DoubleSide}), //Front
+  new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load('img/2.jpg'), side: THREE.DoubleSide}), //Back
+  new THREE.MeshPhongMaterial({color:0x555555, side: THREE.DoubleSide}),
+  new THREE.MeshPhongMaterial({color:0x555555, side: THREE.DoubleSide}),
+  new THREE.MeshPhongMaterial({color:0x555555, side: THREE.DoubleSide}),
+  new THREE.MeshPhongMaterial({color:0x555555, side: THREE.DoubleSide})
+];
+
+
+//var material = new THREE.MeshFaceMaterial(cubeMaterials);
+var cube = new THREE.Mesh( geometry, cubeMaterials );
+cube.castShadow = true;
+//end CUBE
+//scene.add(cube);
+
+var combined = new THREE.Geometry();
+THREE.GeometryUtils.merge( combined, cube );
+THREE.GeometryUtils.merge( combined, cylinder );
+var mesh = new THREE.Mesh( combined, cubeMaterials );
+mesh.geometry.faces[12].materialIndex = 6;
+mesh.geometry.faces[13].materialIndex = 6;
+mesh.geometry.faces[14].materialIndex = 6;
+mesh.geometry.faces[15].materialIndex = 6;
+mesh.geometry.faces[16].materialIndex = 6;
+mesh.geometry.faces[17].materialIndex = 6;
+mesh.geometry.faces[18].materialIndex = 6;
+mesh.geometry.faces[19].materialIndex = 6;
+//console.log(mesh.geometry.faces.length);
+//console.log(mesh.geometry);
+cube = mesh.clone();
+cube.castShadow = true;
+scene.add(cube);
+//cube.scale.z = 2;
 
 //Light needed for MeshPhongMaterial!!
 var light = new THREE.DirectionalLight( 0xffffff, .4 );
 light.position.set( 0, 1, 1 ).normalize();
 scene.add(light);
-
-//var material = new THREE.MeshBasicMaterial( { color: "#433F81", wireframe:false } );
-var material = new THREE.MeshFaceMaterial(cubeMaterials);
-var cube = new THREE.Mesh( geometry, material );
-
-cube.castShadow = true;
-
-//cube.rotation.x += 0;
-//cube.rotation.y += .8;
-//cube.rotation.z += .7;
-scene.add( cube );
-
 
 //Lighting stuff
 var ambientLight = new THREE.AmbientLight(0x9F9F9F, 0.7);
