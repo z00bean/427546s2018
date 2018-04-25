6f3795cb9017ba9253bc5cdec90536f209f162ce
Zubin Bhuyan <zoo_bean_13@yahoo.co.in>
2018-04-25 02:05:39 -0400

    Update cgProject.js

--- finalProject/js/cgProject.js
+++ finalProject/js/cgProject.js
@@ -1,41 +1,135 @@
+var c1x1 = 90, c1y1 = 115;
+var scaleX = 1, scaleY = 1;
+var scaleChanged = 0;
+
+function mouseDownC1(canvas, evt) {
+  var rect = canvas.getBoundingClientRect();
+  c1x1 = evt.clientX - rect.left;
+  c1y1 = evt.clientY - rect.top;
+
+  canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+
+  canvas.getContext('2d').fillStyle="#f0f0fa";
+  ctx1.fillRect(0, 0, c1.width, c1.height);
+  var img1 = new Image();
+  img1.onload = function() {
+    //ctx1.drawImage(img1, 0, 0, c1x1*300/500, c1y1*150/250);
+    ctx1.drawImage(img1, 0, 0, c1x1 * 300/500, canvas.height-10);
+  };
+  img1.src = 'img/ssfront.png';
+  scaleX = (c1x1 * 300/500) / (canvas.width/2.5);
+  scaleChanged = 1;
+  console.log("scaleX: "+scaleX);
+  console.log("X:"+c1x1 + " Y: " + c1y1);
+  console.log("canvas width:"+canvas.width + " canvas height: " + canvas.height);
+  ctx1.fillStyle="#000000";
+  ctx1.font = "9px Arial";
+  ctx1.fillText("Front View",255,10);
+
+  updateTopView();
+}
+
 //Front
 var c1=document.getElementById("canvas1");
+
+c1.addEventListener("mousedown", function(event){
+  mouseDownC1(c1, event);
+});
 var ctx1=c1.getContext("2d");
+ctx1.fillStyle="#f0f0fa";
+ctx1.fillRect(0, 0, c1.width, c1.height);
 var img1 = new Image();
-ctx1.font = "11px Arial";
-ctx1.fillText("Front view",10,10);
+ctx1.fillStyle="#000000";
+ctx1.font = "9px Arial";
+ctx1.fillText("Front View",255,10);
 img1.onload = function() {
 
-  ctx1.drawImage(img1, 40, 40, 50, 75);
+  ctx1.drawImage(img1, 0, 0, c1.width/2.5, c1.height-10);
 
 };
-img1.src = '../img/1.jpg';
+img1.src = 'img/ssfront.png';
+
+//Side handlers
+var c2x1 = 90, c2y1 = 115;
+
+function mouseDownC2(canvas, evt) {
+  var rect = canvas.getBoundingClientRect();
+  c2x1 = evt.clientX - rect.left;
+  c2y1 = evt.clientY - rect.top;
+
+  canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+
+  canvas.getContext('2d').fillStyle="#f0f0fa";
+  ctx2.fillRect(0, 0, c1.width, c1.height);
+  var img2 = new Image();
+  img2.onload = function() {
+    //ctx1.drawImage(img1, 0, 0, c1x1*300/500, c1y1*150/250);
+    ctx2.drawImage(img2, 0, 0, c2x1 * 300/500, canvas.height-10);
+  };
+  img2.src = 'img/ssside.png';
+  scaleY = (c2x1 * 300/500) / (canvas.width/2.5);
+  scaleChanged = 1;
+  console.log("scaleY: "+scaleY);
+  console.log("C2 X:"+c2x1 + " C2 Y: " + c2y1);
+  console.log("canvas width:"+canvas.width + " canvas height: " + canvas.height);
+  ctx2.fillStyle="#000000";
+  ctx2.font = "9px Arial";
+  ctx2.fillText("Side view",255,10);
+
+  updateTopView();
+}
 
 //Side
 var c2=document.getElementById("canvas2");
-var ctx2=c2.getContext("2d");
-ctx2.font = "11px Arial";
-ctx2.fillText("Side view",10,10);
+
+c2.addEventListener("mousedown", function(event){
+  mouseDownC2(c2, event);
+});
+var ctx2 = c2.getContext("2d");
+ctx2.fillStyle="#f0f0fa";
+ctx2.fillRect(0, 0, c2.width, c2.height);
 var img2 = new Image();
+ctx2.fillStyle="#000000";
+ctx2.font = "9px Arial";
+ctx2.fillText("Side view",255,10);
 img2.onload = function() {
 
-  ctx2.drawImage(img2, 40, 40, 50, 75);
+  ctx2.drawImage(img2, 0, 0, c2.width/2.5, c2.height-10);
 
 };
-img2.src = '../img/2.jpg';
+img2.src = 'img/ssside.png';
+
 
 //Top
+function updateTopView() {
+  c3.getContext('2d').clearRect(0, 0, c3.width, c3.height);
+
+  c3.getContext('2d').fillStyle="#f0f0fa";
+  ctx3.fillRect(0, 0, c3.width, c3.height);
+  var img3 = new Image();
+  img3.onload = function() {
+    //ctx1.drawImage(img1, 0, 0, c1x1*300/500, c1y1*150/250);
+    ctx3.drawImage(img3, 0, 0, c3.width/3 * scaleX, c3.width/3 * scaleY);
+  };
+  img3.src = 'img/sstop.png';
+  ctx3.fillStyle="#000000";
+  ctx3.font = "9px Arial";
+  ctx3.fillText("Top view",255,10);
+}
 var c3=document.getElementById("canvas3");
 var ctx3=c3.getContext("2d");
-ctx3.font = "11px Arial";
-ctx3.fillText("Top view",10,10);
+ctx3.fillStyle="#f0f0fa";
+ctx3.fillRect(0, 0, c3.width, c3.height);
+ctx3.fillStyle="#000000";
+ctx3.font = "9px Arial";
+ctx3.fillText("Top view",255,10);
 var img3 = new Image();
 img3.onload = function() {
 
-  ctx3.drawImage(img3, 40, 20);
+  ctx3.drawImage(img3, 10, 0, c3.width/3, c3.width/3);
 
 };
-img3.src = '../img/top.png';
+img3.src = 'img/sstop.png';
 
 //end side, top views
 
@@ -50,15 +144,6 @@ var renderer = new THREE.WebGLRenderer({antialias:true});
 renderer.shadowMap.enabled = true;
 renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 //renderer.shadowMapType = THREE.PCFShadowMap; 
-/*
-renderer.shadowCameraNear = 3;
-renderer.shadowCameraFar = camera.far;
-renderer.shadowCameraFov = 50;
-renderer.shadowMapBias = 0.0039;
-renderer.shadowMapDarkness = 0.5;
-renderer.shadowMapWidth = 1024;
-renderer.shadowMapHeight = 1024;
-*/
 
 var rotateStatus = 0;
 var ambLightStatus = 1;
@@ -84,14 +169,14 @@ var Syx = 0,
 // renderer clear color
 renderer.setClearColor("#d0d0fa");
 //renderer.setSize( document.getElementById('4').style.width, document.getElementById('4').style.height );
-renderer.setSize( window.innerWidth/4, window.innerHeight/4 );
+renderer.setSize( window.innerWidth/2.8, window.innerHeight/2.8 );
 renderer.shadowMap.enabled = true;
 
 window.addEventListener('resize', function(){
   //var width = document.getElementById('4').style.width;
   //var height = document.getElementById('4').style.height;
-  var width = window.innerWidth/4;
-  var height = window.innerHeight/4;
+  var width = window.innerWidth/2.8;
+  var height = window.innerHeight/2.8;
   renderer.setSize(width, height);
   camera.aspect = width/height;
   camera.updateProjectionMatrix();
@@ -165,26 +250,38 @@ function setCameraType(){
   };
 
 function setShearStatusXY(){
-  if(document.getElementById("chkShearXY").checked == true)
+  if(document.getElementById("chkShearXY").checked == true){
     Sxy = 1;
-  else
+    //document.getElementById("chkShearZX").disabled = true;
+  }
+  else{
     Sxy = -1;
+    //document.getElementById("chkShearZX").disabled = false;
+  }
 
   shearChanged = 1;
 };
 function setShearStatusYZ(){
-  if(document.getElementById("chkShearYZ").checked == true)
+  if(document.getElementById("chkShearYZ").checked == true){
     Syz = 1;
-  else
+    //document.getElementById("chkShearXY").disabled = true;
+  }
+  else{
     Syz = -1;
+    //document.getElementById("chkShearXY").disabled = false;
+  }
 
   shearChanged = 1;
 };
 function setShearStatusZX(){
-  if(document.getElementById("chkShearZX").checked == true)
+  if(document.getElementById("chkShearZX").checked == true){
     Szx = 1;
-  else
+    //document.getElementById("chkShearXY").disabled = true;
+  }
+  else{
     Szx = -1;
+    //document.getElementById("chkShearXY").disabled = false;
+  }
 
   shearChanged = 1;
 };
@@ -198,18 +295,18 @@ container.appendChild(renderer.domElement);
 
 //document.body.appendChild( renderer.domElement );
 
-
 //Create a plane that receives shadows (but does not cast them)
 //Create grass
+
 var grassTex = THREE.ImageUtils.loadTexture('img/grass.jpg'); 
 grassTex.wrapS = THREE.RepeatWrapping; 
 grassTex.wrapT = THREE.RepeatWrapping; 
-grassTex.repeat.x = 300; 
-grassTex.repeat.y = 225;
+grassTex.repeat.x = 2; 
+grassTex.repeat.y = 3;
 
 var planeGeometry = new THREE.PlaneBufferGeometry( 20, 20, 32, 32 );
 //var planeMaterial = new THREE.MeshLambertMaterial( { color: 0x009090, side: THREE.DoubleSide } )
-var planeMaterial = new THREE.MeshLambertMaterial( { map: grassTex, side: THREE.DoubleSide } )
+var planeMaterial = new THREE.MeshLambertMaterial( { map: grassTex, side: THREE.DoubleSide } );
 var plane = new THREE.Mesh( planeGeometry, planeMaterial );
 plane.receiveShadow = true;
 plane.rotation.x = -0.5 * Math.PI;
@@ -218,36 +315,63 @@ plane.position.y = -1.25;
 scene.add( plane );
 
 
+//ROOF
+var geometry = new THREE.CylinderGeometry( 0, 1.75, 1, 4 );
+var material = new THREE.MeshPhongMaterial( {color: 0xffff00 , wireframe:false, side: THREE.DoubleSide} );
+var cylinder = new THREE.Mesh( geometry, material );
+cylinder.rotation.y = 0.25 * Math.PI;
+cylinder.position.y = 1.75;
+cylindercastShadow = true;
+//scene.add( cylinder );
+//end-ROOF
 
-// cube setup
+// CUBE setup
 var geometry = new THREE.BoxGeometry( 2, 2.5, 2 );
 
+var brickTex = THREE.ImageUtils.loadTexture('img/brick.jpg'); 
+brickTex.wrapS = THREE.RepeatWrapping; 
+brickTex.wrapT = THREE.RepeatWrapping; 
+brickTex.repeat.x = 3; 
+brickTex.repeat.y = 3;
 var cubeMaterials = [
   new THREE.MeshPhongMaterial({map: new THREE.TextureLoader().load('img/3.jpg'), side: THREE.DoubleSide}), //Right
-  new THREE.MeshPhongMaterial({map: new THREE.TextureLoader().load('img/2.jpg'), side: THREE.DoubleSide}), //Left
-  new THREE.MeshBasicMaterial({color:0xF08F81, side: THREE.DoubleSide}), //Top
-  new THREE.MeshPhongMaterial({color:0x433F81, side: THREE.DoubleSide}), //Bottom
-  new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load('img/1.jpg'), side: THREE.DoubleSide}), //Front
-  new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load('img/3.jpg'), side: THREE.DoubleSide}) //Back
+  new THREE.MeshPhongMaterial({map: brickTex, side: THREE.DoubleSide}), //Left
+  new THREE.MeshBasicMaterial({color: 0xF0aa0a, side: THREE.DoubleSide}), //Top
+  new THREE.MeshPhongMaterial({color:0xFFFFFF, side: THREE.DoubleSide}), //Bottom
+  new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load('img/2.jpg'), side: THREE.DoubleSide}), //Front
+  new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load('img/2.jpg'), side: THREE.DoubleSide}), //Back
+  new THREE.MeshPhongMaterial({color:0x555555, side: THREE.DoubleSide}),
+  new THREE.MeshPhongMaterial({color:0x00FFFF, side: THREE.DoubleSide}),
+  new THREE.MeshPhongMaterial({color:0x00a0FF, side: THREE.DoubleSide}),
+  new THREE.MeshPhongMaterial({color:0x4040aF, side: THREE.DoubleSide})
 ];
 
+//var material = new THREE.MeshFaceMaterial(cubeMaterials);
+var cube = new THREE.Mesh( geometry, cubeMaterials );
+cube.castShadow = true;
+//end CUBE
+//scene.add(cube);
 
+var combined = new THREE.Geometry();
+THREE.GeometryUtils.merge( combined, cube );
+THREE.GeometryUtils.merge( combined, cylinder );
+var mesh = new THREE.Mesh( combined, cubeMaterials );
+//Pyramid(Cylinder) roof color
+mesh.geometry.faces[12].materialIndex = 6;
+mesh.geometry.faces[13].materialIndex = 6;
+mesh.geometry.faces[14].materialIndex = 6;
+mesh.geometry.faces[15].materialIndex = 6;
+mesh.geometry.faces[16].materialIndex = 6;
+//console.log(mesh.geometry.faces.length);
+//console.log(mesh.geometry);
+cube = mesh.clone();
+cube.castShadow = true;
+scene.add(cube);
+//cube.scale.z = 3;
 //Light needed for MeshPhongMaterial!!
 var light = new THREE.DirectionalLight( 0xffffff, .4 );
 light.position.set( 0, 1, 1 ).normalize();
 scene.add(light);
-
-//var material = new THREE.MeshBasicMaterial( { color: "#433F81", wireframe:false } );
-var material = new THREE.MeshFaceMaterial(cubeMaterials);
-var cube = new THREE.Mesh( geometry, material );
-
-cube.castShadow = true;
-
-//cube.rotation.x += 0;
-//cube.rotation.y += .8;
-//cube.rotation.z += .7;
-scene.add( cube );
-
 
 //Lighting stuff
 var ambientLight = new THREE.AmbientLight(0x9F9F9F, 0.7);
@@ -277,6 +401,12 @@ var helper = new THREE.CameraHelper( light.shadow.camera );
 //scene.add( helper );
 
 var update = function(){
+
+  if(scaleChanged == 1){
+    scaleChanged = 0;
+    cube.scale.z = scaleX;
+    cube.scale.x = scaleY;
+  }
 
   if(helperStatus == 1){
     scene.add( helper );
@@ -313,7 +443,7 @@ var update = function(){
 
   if(shearChanged == 1){
     shearChanged = 0;
-    scene.remove( cube );
+    scene.remove(cube);
     var matrix = new THREE.Matrix4();
 
     matrix.set(   1,   Syx,  Szx,  0,
@@ -323,7 +453,7 @@ var update = function(){
 
     // apply shear matrix to geometry                  
     cube.geometry.applyMatrix( matrix );
-    scene.add( cube );
+    scene.add(cube);
     Sxy = 0;
     Syz = 0;
     Szx = 0;
